ac33c818c1a30667ede7ca73d90efecdaf331edc
DESKTOP-N4F2CA1\Admin <endee1106@gmail.com>
2022-07-13 08:56:57 +0700

    home

--- src/component/content/ListTest.js
+++ src/component/content/ListTest.js
@@ -1,10 +1,30 @@
 import React from "react";
 import '../../css/layout/HomeContent.css';
+import TestApi from "../../api/entities/TestApi.js"
+import { useEffect, useState } from "react"
 
-function ListTest(){
-    return( <div className="content">
-                <h1>Danh sách bài thi cho mình</h1>
-            </div> );
+
+
+function ListTest() {
+    const [sections, setSections] = useState([]);
+    
+    useEffect(() => {
+         TestApi.getListTest(100,0,'').then(res => {
+            setSections(res.data.data.data)
+        }).catch(err => {
+            console.log("Có lỗi vui lòng reload lại trang");
+        });
+        
+    },[]);
+    const sectionList = sections.map(sections => (
+        <li key={sections.id}>{sections.name}</li>
+    )) 
+    return (<div className="content">
+        <h2>Danh sách bài thi</h2>
+        <div className="section-list">
+            {sectionList}
+        </div>
+    </div>);
 }
 
 export default ListTest;
